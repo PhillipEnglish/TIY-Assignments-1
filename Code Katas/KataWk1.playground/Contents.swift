78da74773cbd1b1805ffd50698e8417104b46059
Jen Hamilton <embryconcepts@gmail.com>
2015-10-05 16:21:39 -0400

    prob 1 complete

--- Code Katas/KataWk1.playground/Contents.swift
+++ Code Katas/KataWk1.playground/Contents.swift
@@ -1,14 +1,15 @@
-//Question 1
-//
-//Multiples of 3 and 5
-//
-//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+// Question 1 - Multiples of 3 and 5
 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
+var sum = 0
+for i in 1..<1000 {
+    if i % 3 == 0 || i % 5 == 0 {
+        sum += i
+    }
+}
+sum
 
+// Question 2 - Largest Palindrome Product
 
-//Question 2
-//
-//Largest Palindrome Product
-//
-//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.
