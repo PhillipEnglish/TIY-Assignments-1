d96d03e33992e94a287d4b488b3737680fb5789e
Jen Hamilton <embryconcepts@gmail.com>
2015-10-06 11:25:19 -0400

    created initial class

--- In Class Projects/TipCalculator.playground/Contents.swift
+++ In Class Projects/TipCalculator.playground/Contents.swift
@@ -14,3 +14,41 @@ tipAndTotalNamed.tipAmt
 let total = 21.09
 let taxPercent = 0.06
 let subTotal = total / (taxPercent + 1)
+
+func calcTipWithTipPercent(tipPercent: Double) -> (tipAmount: Double, finalTotal: Double)
+{
+    let tipAmount = subTotal * tipPercent
+    let finalTotal = total + tipAmount
+    return (tipAmount, finalTotal)
+}
+
+let totalWithTip = calcTipWithTipPercent(0.20)
+
+
+class TipCalculator
+{
+    let total: Double
+    let taxPct: Double
+    let subTotal: Double
+    
+    init(argTotal: Double, argTaxPct: Double)
+    {
+        self.total = argTotal
+        self.taxPct = argTaxPct
+        self.subTotal = total / (taxPct + 1)
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double
+    {
+        return subTotal * tipPct
+    }
+    
+}
+
+let tipCalc = TipCalculator(argTotal: 83.25, argTaxPct: 0.06)
+let tip20 = tipCalc.calcTipWithTipPct(0.2)
+tipCalc.subTotal
+tipCalc.total
+tip20
+
+
