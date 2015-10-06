f23ed63926e6bc6de7698a7a497f76224881d9ae
Jen Hamilton <embryconcepts@gmail.com>
2015-10-06 13:11:08 -0400

    finished up

--- In Class Projects/TipCalculator.playground/Contents.swift
+++ In Class Projects/TipCalculator.playground/Contents.swift
@@ -43,12 +43,47 @@ class TipCalculator
         return subTotal * tipPct
     }
     
+    func printPossibleTips()
+    {
+        print("15%: $\(calcTipWithTipPct(0.15))")
+        print("20%: $\(calcTipWithTipPct(0.20))")
+        print("25%: $\(calcTipWithTipPct(0.25))")
+    }
+    
+    func returnPossibleTips() -> [Int: Double]
+    {
+        let possibleTips = [0.15, 0.18, 0.20]
+        var rValue = [Int: Double]()
+        for possibleTip in possibleTips
+        {
+            let intPct = Int(possibleTip*100)
+            rValue[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return rValue
+    }
 }
 
-let tipCalc = TipCalculator(argTotal: 83.25, argTaxPct: 0.06)
+let tipCalc = TipCalculator(argTotal: 45.75, argTaxPct: 0.06)
 let tip20 = tipCalc.calcTipWithTipPct(0.2)
 tipCalc.subTotal
 tipCalc.total
 tip20
 
+tipCalc.printPossibleTips()
+let tipDictionary = tipCalc.returnPossibleTips()
+tipDictionary[20]
 
+class BodyPart
+{
+    
+}
+
+class Limb : BodyPart
+{
+    
+}
+
+class Leg : Limb 
+{
+    
+}
