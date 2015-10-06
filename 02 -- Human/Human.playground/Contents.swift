d65b8ffbcd23f8e9fca984f070f4570844a6811c
Jen Hamilton <embryconcepts@gmail.com>
2015-10-06 13:30:01 -0400

    classes outlined

--- 02 -- Human/Human.playground/Contents.swift
+++ 02 -- Human/Human.playground/Contents.swift
@@ -1,17 +1,608 @@
 class BodyPart
 {
-    init(){
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
         
     }
 }
 
-class Limb : BodyPart
+class Head : BodyPart
 {
+    let x: String
+    let y: Bool
+    let z: String
     
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Hand : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Foot : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Foot : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Finger : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Toe : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
 }
 
 class Leg : Limb
 {
+    let x: String
+    let y: Bool
+    let z: String
     
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
 }
 
+class Arm : Limb
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Organ : BodyPart
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Heart : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Liver : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Kidney : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Skin : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Lungs : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Stomach : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Gallbladder : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Appendix : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Pancreas : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Bladder : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
+
+class Eye : Organ
+{
+    let x: String
+    let y: Bool
+    let z: String
+    
+    
+    init(argX: String, argY: Bool, argZ: String){
+        self.x = argX
+        self.y = argY
+        self.z = argZ
+    }
+    
+    func a()
+    {
+        
+    }
+    
+    func b()
+    {
+        
+    }
+    
+    func c()
+    {
+        
+    }
+}
