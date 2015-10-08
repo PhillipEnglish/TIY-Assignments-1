68f927880b8784ada952ab908bf29730c6cc2d25
Jen Hamilton <embryconcepts@gmail.com>
2015-10-07 20:44:26 -0400

    textView still not displaying

--- 03 -- MissionBriefing/MissionBriefing/MissionBriefingViewController.swift
+++ 03 -- MissionBriefing/MissionBriefing/MissionBriefingViewController.swift
@@ -7,14 +7,14 @@
 //
 import UIKit
 
-class MissionBriefingViewController: UIViewController, UITextFieldDelegate
+class MissionBriefingViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate
 {
     // Place IBOutlet properties below
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var authenticateButton: UIButton!
     @IBOutlet var greetingLabel: UILabel!
-    @IBOutlet var missionTextView: UITextField!
+    @IBOutlet var missionTextView: UITextView!
     
     
     override func viewDidLoad()
@@ -29,7 +29,7 @@ class MissionBriefingViewController: UIViewController, UITextFieldDelegate
         nameTextField.text = ""
         passwordTextField.text = ""
         greetingLabel.text = ""
-        missionTextView.text = ""
+//        missionTextView.text = ""
     }
     
     override func didReceiveMemoryWarning()
@@ -51,6 +51,14 @@ class MissionBriefingViewController: UIViewController, UITextFieldDelegate
         return authenticateAgent()
     }
     
+    // MARK: - UITextView Delegate
+    
+    func textViewShouldReturn(textView: UITextView) -> Bool
+    {
+        return authenticateAgent()
+    }
+
+    
     // MARK: - Private
     
     func authenticateAgent() -> Bool
@@ -69,20 +77,28 @@ class MissionBriefingViewController: UIViewController, UITextFieldDelegate
                 // #5
                 greetingLabel.text = "Good evening, Agent \(lastName)"
                 // #6 TODO not working
-                missionTextView.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
+//                missionTextView.text = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
                 // #7
+                view.backgroundColor = UIColor(red: 0.585, green: 0.78, blue: 0.188, alpha: 1.0)
             }
             else
             {
                 greetingLabel.text = "Please enter your password."
+                agentFailureToAuthenticate()
             }
         }
         else
         {
             // #8
+            agentFailureToAuthenticate()
         }
         
         return rc
+    }
+    
+    func agentFailureToAuthenticate()
+    {
+        view.backgroundColor = UIColor(red: 0.78, green: 0.188, blue: 0.188, alpha: 1.0)
     }
 }
 
@@ -90,26 +106,4 @@ class MissionBriefingViewController: UIViewController, UITextFieldDelegate
 // 6. The mission briefing textview needs to be populated with the briefing from HQ, but it must also include the last
 //    name of the agent that logged in. Perhaps you could use the text in the textfield to get the agent's last name.
 //    How would you inject that last name into the paragraph of the mission briefing?
-
-
-
-// 7. The view's background color needs to switch to green to indicate a successful login by the agent.
-//
-//    The color's RGB value is Red: 0.585, Green: 0.78, Blue: 0.188 with an alpha of 1. There is a class method on the
-//    UIColor class that returns a color object with custom defined RGBA values. Open the documentation and look for a
-//    method on UIColor that can take red, green, blue and alpha values as arguments.
-//
-//    Once you have the color object, you should be able to set the view's background color to this object.
-
-//
-// 8. The view's background color needs to switch to red to indicate a failed login by the agent.
-//
-//    The color's RGB value is Red: 0.78, Green: 0.188, Blue: 0.188 with an alpha of 1. There is a class method on the
-//    UIColor class that returns a color object with custom defined RGBA values. Open the documentation and look for a
-//    method on UIColor that can take red, green, blue and alpha values as arguments.
-//
-//    Once you have the color object, you should be able to set the view's background color to this object.
-//
-
-
             
