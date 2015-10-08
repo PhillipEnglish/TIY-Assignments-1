641de1cdd4a5fbd9574240fa39231e005145a6ac
Jen Hamilton <embryconcepts@gmail.com>
2015-10-08 15:52:29 -0400

    added iboutlets

--- 04 -- OutaTime/OutaTime/OutaTime/TimePickerViewController.swift
+++ 04 -- OutaTime/OutaTime/OutaTime/TimePickerViewController.swift
@@ -1,6 +1,5 @@
-
 //
-//  TimeCircuitsViewController.swift
+//  TimePickerViewController.swift
 //  OutaTime
 //
 //  Created by Jennifer Hamilton on 10/8/15.
@@ -9,22 +8,18 @@
 
 import UIKit
 
-class TimeCircuitsViewController: UIViewController {
 
+class TimePickerViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet var destinationTimeLabel: UILabel!
-    @IBOutlet var presentTimeLabel: UILabel!
-    @IBOutlet var lastTimeDepartedLabel: UILabel!
-    @IBOutlet var speedLabel: UILabel!
+    
     
 }
-
