fd37216403a96f8e249906b07ce8bb302a4297e9
Jen Hamilton <embryconcepts@gmail.com>
2015-10-08 18:49:11 -0400

    cleared labels prior to setting - errors

--- 04 -- OutaTime/OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ 04 -- OutaTime/OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -12,31 +12,37 @@ import UIKit
 class TimeCircuitsViewController: UIViewController 
 {
     let dateFormatter = NSDateFormatter()
+    
+    @IBOutlet var destinationTimeLabel: UILabel!
+    @IBOutlet var presentTimeLabel: UILabel!
+    @IBOutlet var lastTimeDepartedLabel: UILabel!
+    @IBOutlet var speedLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // useful source: http://www.codingexplorer.com/swiftly-getting-human-readable-date-nsdateformatter/
         dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("MMM dd yyyy", options: 0, locale: NSLocale(localeIdentifier: "en-US"))
+        
+        destinationTimeLabel.text = ""
+        presentTimeLabel.text = ""
+        lastTimeDepartedLabel.text = ""
+        speedLabel.text = ""
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet var destinationTimeLabel: UILabel!
-    @IBOutlet var presentTimeLabel: UILabel!
-    @IBOutlet var lastTimeDepartedLabel: UILabel!
-    @IBOutlet var speedLabel: UILabel!
-    
-    presentTimeLabel.text = dateFormatter(NSDate())
     
     var test = "test"
     test = "help"
     
+    presentTimeLabel.text = dateFormatter(NSDate())
+    
     var currentSpeed = 0
-    speedLabel.text = "\(currentSpeed ) + MPH"
+    speedLabel.text = "\(currentSpeed) MPH"
     
     lastTimeDepartedLabel.text = dateFormatter(NSDate())
     
