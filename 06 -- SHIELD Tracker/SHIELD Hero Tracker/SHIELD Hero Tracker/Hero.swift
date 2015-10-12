8c2153996e348f655823e5bfb1e72117a84d3894
Jen Hamilton <embryconcepts@gmail.com>
2015-10-12 14:25:46 -0400

    created and linked classes for views and Hero

---
+++ 06 -- SHIELD Tracker/SHIELD Hero Tracker/SHIELD Hero Tracker/Hero.swift
@@ -0,0 +1,9 @@
+//
+//  Hero.swift
+//  SHIELD Hero Tracker
+//
+//  Created by Jennifer Hamilton on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
---
+++ 06 -- SHIELD Tracker/SHIELD Hero Tracker/SHIELD Hero Tracker/HeroDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HeroDetailViewController.swift
+//  SHIELD Hero Tracker
+//
+//  Created by Jennifer Hamilton on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
