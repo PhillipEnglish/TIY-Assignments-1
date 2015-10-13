6b02bb7c1dca755fc23cc911adfd2463723b2f83
Jen Hamilton <embryconcepts@gmail.com>
2015-10-13 16:04:59 -0400

    fixed Add button
    still working on sending string from Mode to VC

--- 07 -- Jackpot/Jackpot/Jackpot/LotteryTicket.swift
+++ 07 -- Jackpot/Jackpot/Jackpot/LotteryTicket.swift
@@ -14,16 +14,20 @@ class LotteryTicket
      var ticketNumbers = Array<Int>()
     
     //initialize with an array of 6 unique numbers between 1 and 53
-    init(num: Int)
+    init()
     {
         var arrayOfNum: Array<Int> = []
+//        var ticketNumbers = Array<Int>()
+        var ticketString: String
         
         var i = 0
         
         //limit it to 6 numbers
         if i <= 6
         {
+            var num = generateNumber()
             if arrayOfNum.contains(num)
+                // if num is already in array, generate a new num
             {
                 generateNumber()
             }
@@ -35,14 +39,21 @@ class LotteryTicket
             }
         }
         
-        ticketNumbers = arrayOfNum
+        ticketString = String(arrayOfNum)
+//        ticketNumbers = arrayOfNum
     }
     
     // Source: http://forums.macrumors.com/threads/generating-random-numbers-with-xcode-7-and-swift-2.1915674/
-    func generateNumber() -> UInt
+    func generateNumber() -> Int
     {
-        var randomNum: UInt = 0
-        arc4random_buf(&randomNum, sizeof(UInt))
+        var randomNum: Int = 0
+        arc4random_buf(&randomNum, sizeof(Int))
+        
         return 1 + (randomNum % (53 - 1))
+        
+        // from Counter app, not as precise as above
+//        return Int(arc4random() % 53)
     }
+    
+
 }
