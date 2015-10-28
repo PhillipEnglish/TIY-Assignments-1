5afe834a67d99edba8b9d4bd4f6c53188a42adec
Jen Hamilton <embryconcepts@gmail.com>
2015-10-28 11:35:19 -0400

    moved code into separate functions

--- 18 -- MuttCutts/MuttCutts/MapViewController.swift
+++ 18 -- MuttCutts/MuttCutts/MapViewController.swift
@@ -16,10 +16,43 @@ class MapViewController: UIViewController
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var distanceLabel: UILabel!
     
+    let tiyOrlando = CLLocationCoordinate2DMake(28.540923, -81.38216)
+    let tiyOrlandoAnnotation = MKPointAnnotation()
+    let tiyTampa = CLLocationCoordinate2DMake(27.770068, -82.63642)
+    let tiyTampaAnnotation = MKPointAnnotation()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        distanceLabel.hidden = true
+        setLocations()
+        mapLocations()
+        calculateDistance()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Setup
+    
+     func setupView()
+    {
+    
+    }
+    
+    // MARK: - Action Handlers
+    
+    
+    // MARK: - Popover Delegate
+    
+    
+    // MARK: - Private Functions
+    
+    private func getLocations()
+    {
         // geocoding
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString("Lakeland, FL", completionHandler: {(placemarks: [CLPlacemark]?, error: NSError?) -> Void in
@@ -31,46 +64,42 @@ class MapViewController: UIViewController
                 self.mapView.addAnnotation(annotation)
             }
         })
-        
+    }
+    
+    private func setLocations()
+    {
         // map creation:
-        let tiyOrlando = CLLocationCoordinate2DMake(28.540923, -81.38216)
-        let tiyOrlandoAnnotation = MKPointAnnotation()
         tiyOrlandoAnnotation.coordinate = tiyOrlando
         tiyOrlandoAnnotation.title = "The Iron Yard"
         tiyOrlandoAnnotation.subtitle = "Orlando"
         
-        let tiyTampa = CLLocationCoordinate2DMake(27.770068, -82.63642)
-        let tiyTampaAnnotation = MKPointAnnotation()
         tiyTampaAnnotation.coordinate = tiyTampa
         tiyTampaAnnotation.title = "The Iron Yard"
         tiyTampaAnnotation.subtitle = "Tampa"
-        
+
+    }
+    
+    private func mapLocations()
+    {
+//        let viewRegion = MKCoordinateRegionMakeWithDistance(tiyOrlando, 2000, 2000)
+//        mapView.setRegion(viewRegion, animated: true)
+
         let annotations = [tiyOrlandoAnnotation,tiyTampaAnnotation]
         mapView.addAnnotations(annotations)
         mapView.showAnnotations(annotations, animated: true)
         // zoom out enough to show the annotations
         mapView.camera.altitude *= 2
-        
-        
-//        let viewRegion = MKCoordinateRegionMakeWithDistance(tiyOrlando, 2000, 2000)
-//        mapView.setRegion(viewRegion, animated: true)
-        
+    }
+    
+    private func calculateDistance()
+    {
         // creating a location object
         let orlandoLocation = CLLocation(coordinate: tiyOrlando, altitude: 0, horizontalAccuracy: 0, verticalAccuracy: 0, timestamp: NSDate())
         let tampaLocation = CLLocation(coordinate: tiyTampa, altitude: 0, horizontalAccuracy: 0, verticalAccuracy: 0, timestamp: NSDate())
         let lineOfSightDistance = orlandoLocation.distanceFromLocation(tampaLocation)
+        distanceLabel.hidden = false
         distanceLabel.text = ("Distance between \(tiyOrlandoAnnotation.subtitle!) and \(tiyTampaAnnotation.subtitle!): " + String(format: "%.2f", lineOfSightDistance * 0.00062137) + " miles")
-        
     }
-
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-
 
 }
 
