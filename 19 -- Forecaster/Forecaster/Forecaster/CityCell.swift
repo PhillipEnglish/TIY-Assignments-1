26d9f8d4b4d3c3090facf245d7b4bc75464a375d
Jen Hamilton <embryconcepts@gmail.com>
2015-10-29 11:10:07 -0400

    init commit for 19-Forecaster

---
+++ 19 -- Forecaster/Forecaster/Forecaster/CityCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityCell.swift
+//  Forecaster
+//
+//  Created by Jennifer Hamilton on 10/29/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
